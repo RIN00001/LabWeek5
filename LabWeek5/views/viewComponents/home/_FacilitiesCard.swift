abc926ab5b8dd3f5b6c8697abe8bea0d9c147463
RIN <hamerboy37@gmail.com>
2026-03-12 09:45:55 +0700

    Fixed Home UI to match the PDFs

---
+++ LabWeek5/views/viewComponents/home/_FacilitiesCard.swift
@@ -0,0 +1,18 @@
+//
+//  _FacilitiesCard.swift
+//  LabWeek5
+//
+//  Created by student on 12/03/26.
+//
+
+import SwiftUI
+
+struct _FacilitiesCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    _FacilitiesCard()
+}
