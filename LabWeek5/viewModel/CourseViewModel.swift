a4c0368d7d5335b7924bba92e3b7a2285d3789a9
RIN <hamerboy37@gmail.com>
2026-03-12 10:17:44 +0700

    added viewmodels

---
+++ LabWeek5/viewModel/CourseViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  CourseViewModel.swift
+//  LabWeek5
+//
+//  Created by student on 12/03/26.
+//
+
+import SwiftUI
+
+struct CourseViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CourseViewModel()
+}
